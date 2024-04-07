bcf405678ac7c70b2be573196c46413cc7ee7e15
AzizbekAzimjonovich <azizbekazimjonovich2003@gmail.com>
2024-04-07 22:38:36 +0500

    vazifa

---
+++ src/pages/Create.jsx
@@ -0,0 +1,57 @@
+function Create() {
+  return (
+    <div>
+      <h1 className="text-3xl text-center fond-bold mb-10">
+        Create new recipie
+      </h1>
+      <form className="flex items-center flex-col gap-5">
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Title</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Coocing time</span>
+          </div>
+          <input
+            type="number"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Image URL :</span>
+          </div>
+          <div className="flex gap-3">
+            <input
+              type="url"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <button className="btn btn-secondary">ADD</button>
+          </div>
+        </label>
+
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Method</span>
+          </div>
+          <textarea
+            className="textarea textarea-bordered h-24"
+            placeholder="Bio"
+          ></textarea>
+        </label>
+        <button className="btn btn-secondary  w-full max-w-xs">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Create;
